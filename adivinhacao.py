16ad9f28ed6357ff6bf515ecaeed1af8cc67a1ff
Rafael Santos <rafaelsantosdearaujo@gmail.com>
2021-05-15 16:29:27 -0300

    Realizado tratamento do numero para que seja digitado entre 1 e 100 , e aplicado o uso do continue

--- adivinhacao.py
+++ adivinhacao.py
@@ -10,18 +10,23 @@ total_tentativas = total_de_tentativas
 while(total_de_tentativas > 0):
     # Print trabalhando a interpolação de strings
     print("Tentativa {} de {}" .format(rodada,total_tentativas))
-    chute_str = input("Digite o seu numero : ")
+    chute_str = input("Digite o seu número entre 1 e 100 : ")
     print("Voce digitou  : ", chute_str)
+    chute = int(chute_str)
+    if(chute < 1 or chute > 100 ):
+        print("Você deve digitar um número entre 1 e 100")
+        continue
+
     # Transformando o chute informado em numero inteiro .
-    chute = int (chute_str)
+
     if(numero_secreto==chute):
         print("Voce acertou")
         break
     else:
         if(chute>numero_secreto):
-            print("Voce errou !!! , seu chute foi maior do que o numero secreto")
+            print("Voce errou !!! , seu chute foi maior do que o número secreto")
         if(chute<numero_secreto):
-            print("Voce errou !!! ,  seu chute foi menor do que o numero secreto")
+            print("Voce errou !!! ,  seu chute foi menor do que o número secreto")
     total_de_tentativas = total_de_tentativas - 1
     rodada = rodada +1
 print("Fim de jogo")
