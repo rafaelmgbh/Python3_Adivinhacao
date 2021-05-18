d9a03b54e7e60ede8110c3e8ccc788b09ba30a9f
Rafael Santos <rafaelsantosdearaujo@gmail.com>
2021-05-18 12:31:36 -0300

    Adicionado o nivel de dificuldade

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,15 +4,23 @@ print("**********************************************")
 print("  Advinhação do numero")
 print("**********************************************")
 
+print("Qual nível de dificuldade : ")
+print("[1] Fácil  [2] Médio [3] Dificil")
+nivel = int(input("Define o nivel: "))
 
 numero_secreto = random.randrange(1,101)
-total_de_tentativas = 3
 rodada = 1
-total_tentativas = total_de_tentativas
 
+if (nivel == 1):
+    total_de_tentativas = 20
+elif (nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+total_ = total_de_tentativas # adicionado esta variavel para armazenar o total de tentativas 
 while(total_de_tentativas > 0):
     # Print trabalhando a interpolação de strings
-    print("Tentativa {} de {}" .format(rodada,total_tentativas))
+    print("Tentativa {0} de {1}" .format(rodada,total_))
     chute_str = input("Digite o seu número entre 1 e 100 : ")
     print("Voce digitou  : ", chute_str)
     chute = int(chute_str)
